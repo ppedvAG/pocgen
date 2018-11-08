8bc24b92cf205b42d670951d2fa9720419113c5d
Michael Zöhling <michael.zoehling@outlook.com>
2018-11-08 17:39:59 +0100

    WPF Versuch X

--- ppedv.pocgen/pocgen_WPF/MainWindow.xaml.cs
+++ ppedv.pocgen/pocgen_WPF/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using pocgen.Contracts.Interfaces;
+using pocgen.Contracts.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,17 +14,44 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Unity;
+
+using PowerPoint =  Microsoft.Office.Interop.PowerPoint;
+using Word =  Microsoft.Office.Interop.Word;
+using pocgen.ViewModels.ViewModel;
 
 namespace pocgen_WPF
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : System.Windows.Window
     {
         public MainWindow()
         {
+            unityContainer = new UnityContainer();
+
+            appInstance = new Word.Application();
+            unityContainer.RegisterInstance<IOfficeFileOpener<IWordDocument>>(new WordDocumentOpener(appInstance, new string[] { ".doc", ".docx", ".dot", ".dotx" }));
+            unityContainer.RegisterInstance<IOfficeFileOpener<IPowerPointPresentation>>(new PowerPointPresentationOpener(new string[] { ".ppt", ".pptx" }));
+            unityContainer.RegisterInstance<IMarkdownGenerator>(new MarkdownGenerator());
+            unityContainer.RegisterType<IGenerator, Generator>();
+            unityContainer.RegisterType<IFieldFiller, FieldFiller>();
+
+            DataContext =  new WPFViewModel(
+                unityContainer.Resolve<IOfficeFileOpener<IWordDocument>>(),
+                unityContainer.Resolve<IOfficeFileOpener<IPowerPointPresentation>>(),
+                unityContainer.Resolve<IGenerator>(),
+                appInstance,
+                new List<IGeneratorOption>
+                {
+                        new GeneratorOption("ISBeakAtStart","Seitenumbruch beim Anfang einer Reihe von Bilderfolien"),
+                        new GeneratorOption("ISBreakBetween","Seitenumbrich zwischen einzelnen Bilderfolien"),
+                });
             InitializeComponent();
         }
+        private Word.Application appInstance;
+        public UnityContainer unityContainer { get; set; }
+
     }
 }
