a8d2ecaade1d36c49bf25f4e6ade215de9b9b2f4
Michael Zöhling <michael.zoehling@outlook.com>
2019-01-09 12:10:14 +0100

    Rebuild auf .Net Framework

--- Logic/ppedv.pocgen.Logic/PowerPointHelper.cs
+++ Logic/ppedv.pocgen.Logic/PowerPointHelper.cs
@@ -17,16 +17,16 @@ namespace ppedv.pocgen.Logic
 
         public Presentation OpenPresentation(string filename) => app.Presentations.Open(filename);
         public Presentation CreateNewPresentation(string filename) => app.Presentations.Add(MsoTriState.msoFalse);
-        public void SavePresentationAs(Presentation output,string filename) => output.SaveAs(filename);
+        public void SavePresentationAs(Presentation output, string filename) => output.SaveAs(filename);
 
-        public void ExportAllSlidesAsImage(Presentation presentation,string path)
+        public void ExportAllSlidesAsImage(Presentation presentation, string path)
         {
             int height = Convert.ToInt32(presentation.PageSetup.SlideHeight);
             int width = Convert.ToInt32(presentation.PageSetup.SlideWidth);
             for (int i = 0; i < presentation.Slides.Count; i++)
                 presentation.Slides[i + 1].Export($"{path}\\{i}.png", "PNG", width, height);
         }
-        public void MergePresentationContentIntoNewPresentation(IEnumerable<string> sourceFiles, Presentation destination,int insertAtIndex)
+        public void MergePresentationContentIntoNewPresentation(IEnumerable<string> sourceFiles, Presentation destination, int insertAtIndex)
         {
             destination.ApplyTemplate(sourceFiles.First()); // Template aus der ersten Präsentation übernehmen
             foreach (string file in sourceFiles)
